7b8253360a9a7644c99c0e05605eea74a6063350
Mohamed Alsaee <44772147+mohamed97alsaee@users.noreply.github.com>
2024-04-25 12:42:44 +0200

    timer after splash.

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:todo_app_tec/screens/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,6 +11,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
